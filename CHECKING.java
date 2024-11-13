c2981261187103e18c1ad4fdd153be793f598f1f
zavenr <zaven2003@gmail.com>
2024-11-12 18:55:23 -0700

    Update CHECKING.java

--- CHECKING.java
+++ CHECKING.java
@@ -1,29 +1,26 @@
 //Zaven Ranum
 //CS2050
 
-class CHECKING extends ACCOUNT {
-    private double overdraftLimit;
-    private double cashBack;
+import java.util.Random;
 
-    public CHECKING(String accountName, double balance, String accountID, double overdraftLimit, double cashBack) {
-        super(accountName, balance, accountID);
-        this.overdraftLimit = overdraftLimit;
-        this.cashBack = cashBack;
-    }
+public class Checking extends Account {
+  private double overdraftLimit;
+  private double cashBack;
 
-    public double getOverdraftLimit() {
-        return overdraftLimit;
-    }
-    public void setOverdraftLimit(double overdraftLimit) {
-        this.overdraftLimit = overdraftLimit;
-    }
+  public Checking(String accountName, double balance, String accountID, double overdraftLimit, double cashBack) {
+    super(accountName, balance, accountID);
+    this.overdraftLimit = overdraftLimit;
+    this.cashBack = cashBack;
+  }
 
-    public double getCashBack() {
-        return cashBack;
-    }
-    public void setCashBack(double cashBack) {
-        this.cashBack = cashBack;
-    }
+  public static Checking createRandomChecking() {
+    Random rand = new Random();
+    String accountID = "CHK" + rand.nextInt(100000);
+    return new Checking("Random Checking", rand.nextDouble() * 10000, accountID, rand.nextDouble() * 1000, rand.nextDouble() * 5);
+  }
 
-
+  @Override
+  public String toString() {
+    return super.toString() + ", Overdraft Limit: " + overdraftLimit + ", Cashback: " + cashBack;
+  }
 }
